ae04f096b76c3ad1226f903b13a24465066e99f6
Ayush Nair <ayushnair.noida@gmail.com>
2019-03-22 16:41:55 +0530

    Deleted branch react-d3-tree

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import NavBar from '../../components/Navbar/NavBar';
+import React, { Component } from "react";
+import NavBar from "../../components/Navbar/NavBar";
 // import logo from './logo.svg';
 // import './App.css';
-
-
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-
-        }
+        this.state = {};
     }
     render() {
         return (
