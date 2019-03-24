b6f09f5f92c4047c38b463886f606d150ce34aba
Gautam Padiyar <gautam.padiyar@gmail.com>
2019-03-24 21:48:15 +0530

    recat-tree-graph

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NavBar from '../../components/Navbar/NavBar';
 // import logo from './logo.svg';
 // import './App.css';
+import Tree from 'react-tree-graph';
 
 
 
@@ -10,12 +11,46 @@ class App extends Component {
         super();
         this.state = {
 
+          data : {
+            name: 'Parent',
+            children: [{
+              name: 'Child One'
+            }, {
+              name: 'Child Two'
+            }, {
+              name: 'Child Three'
+            }]
+          }
+
         }
     }
+    onClick = (event, nodeKey) =>  {
+
+      // onCLICK
+      // add your modal
+      alert(nodeKey);
+
+      this.setState({data : {
+          name: 'Parent',
+          children: [{
+            name: 'Child One'
+          }, {
+            name: 'Child Two'
+          }]
+        }})
+  };
+
     render() {
         return (
             <div className="App">
                 <NavBar />
+              <Tree
+                  data={this.state.data}
+                  height={400}
+                  width={600}
+                  gProps={{
+                    onClick: this.onClick
+                  }}/>
             </div>
         );
     }
