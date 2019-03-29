62249f1b98ff6b6ba0daa41fae526189b996270a
Ayush <ayushnair.noida@gmail.com>
2019-03-30 01:15:36 +0530

    Basic integration done

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import { PropTypes } from "prop-types";
-import { Button, Modal, Fade, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import Lists from "../Lists/Lists";
 import InputBar from "../InputBar/InputBar";
 
@@ -8,12 +7,10 @@ class Popup extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modal: false,
+            modal: this.props.clicked,
             nestedModal: false,
             closeAll: false,
             input: "",
-            keyProp: this.props.keyProp,
-            idProp: this.props.idProp,
             children: this.props.data.children
         };
         this.onToggle = this.onToggle.bind(this);
@@ -70,9 +67,10 @@ class Popup extends Component {
         this.setState({ input: event.target.value });
     }
     render() {
+
         return (
             <div>
-                {/* <Button color="danger" onClick={this.onToggle}>{this.props.buttonLabel}</Button> */}
+                <Button color="danger" onClick={this.onToggle}>{this.props.buttonLabel}</Button>
                 <Modal isOpen={this.state.modal} onToggle={this.onToggle} className={this.props.className}>
                     <ModalHeader onToggle={this.onToggle}>{this.props.data.name}</ModalHeader>
                     <ModalBody>
@@ -102,57 +100,4 @@ class Popup extends Component {
     }
 }
 
-Modal.propTypes = {
-    // boolean to control the state of the popover
-    isOpen: PropTypes.bool,
-    autoFocus: PropTypes.bool,
-    // if modal should be centered vertically in viewport
-    centered: PropTypes.bool,
-    // corresponds to bootstrap's modal sizes, ie. 'lg' or 'sm'
-    size: PropTypes.string,
-    // callback for toggling isOpen in the controlling component
-    onToggle: PropTypes.func,
-    role: PropTypes.string, // defaults to "dialog"
-    // used to reference the ID of the title element in the modal
-    labelledBy: PropTypes.string,
-    keyboard: PropTypes.bool,
-    // control backdrop, see http://v4-alpha.getbootstrap.com/components/modal/#options
-    backdrop: PropTypes.oneOfType([
-        PropTypes.bool,
-        PropTypes.oneOf(["static"])
-    ]),
-    // if body of modal should be scrollable when content is long
-    scrollable: PropTypes.bool,
-    // allows for a node/component to exist next to the modal (outside of it). Useful for external close buttons
-    // external: PropTypes.node,
-    // called on componentDidMount
-    onEnter: PropTypes.func,
-    // called on componentWillUnmount
-    onExit: PropTypes.func,
-    // called when done transitioning in
-    onOpened: PropTypes.func,
-    // called when done transitioning out
-    onClosed: PropTypes.func,
-    className: PropTypes.string,
-    wrapClassName: PropTypes.string,
-    modalClassName: PropTypes.string,
-    backdropClassName: PropTypes.string,
-    contentClassName: PropTypes.string,
-    // boolean to control whether the fade transition occurs (default: true)
-    fade: PropTypes.bool,
-    cssModule: PropTypes.object,
-    // zIndex defaults to 1000.
-    zIndex: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    // backdropTransition - controls backdrop transition
-    // timeout is 150ms by default to match bootstrap
-    // see Fade for more details
-    backdropTransition: PropTypes.shape(Fade.propTypes),
-    // modalTransition - controls modal transition
-    // timeout is 300ms by default to match bootstrap
-    // see Fade for more details
-    modalTransition: PropTypes.shape(Fade.propTypes),
-    innerRef: PropTypes.object,
-    // if modal should be destructed/removed from DOM after closing
-    unmountOnClose: PropTypes.bool // defaults to true
-};
 export default Popup;
